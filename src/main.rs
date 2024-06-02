504ee62b94ff73f73cae2363273789b1acb76417
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2024-06-02 12:38:47 +0800

    Check source name across all given titles

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ static TITLES_SOURCE_RIGHT: [&str; 4] = [
     "This is a title - FANGS",
     "This is another title - a title with a dash - FANGS",
     "Hello - FANGS",
-    "123 What 3 . - - - Fangs",
+    "123 What 3 . - - - FANGS",
 ];
 
 // There should be two strategies
@@ -50,30 +50,84 @@ fn strip_titles_multiple(titles: &[&str]) {
     }
 }
 
+/// Locate the identical source name in each of the titles.
+/// Note that this requires the titles to come from the same source,
+/// and for all of the titles to consistently include the source name.
 fn titles_locate_matching_source(titles: &[&str]) -> TitleSource {
-    let t_a = titles[0];
-    let t_b = titles[1];
-
-    let max: usize = if t_a.chars().count() > t_b.chars().count() {
-        t_b.chars().count()
-    } else {
-        t_a.chars().count()
-    };
-
-    let t_a_c: Vec<char> = t_a.chars().collect();
-    let t_b_c: Vec<char> = t_b.chars().collect();
-
-    let mut matching_starts: Vec<char> = Vec::new();
-    let mut matching_ends: Vec<char> = Vec::new();
-    let mut current_char: char = '_';
-    for i in 0..(max + 0) {
-        if t_a_c[t_a_c.len() - i - 1] == t_b_c[t_b_c.len() - i - 1] {
-            // note this puts the characters in reverse order
-            matching_ends.push(t_a_c[t_a_c.len() - i - 1]);
+    let mut max: usize = 0;
+    for title in titles {
+        let count = title.chars().count();
+        if count > max {
+            max = count;
         }
-        if t_a_c[i] == t_b_c[i] {
-            matching_starts.push(t_a_c[i]);
+    }
+
+    println!("Max chars (lowest title length) {}", max);
+
+    let mut matching_starts: Vec<&char> = Vec::new();
+    let mut matching_ends: Vec<&char> = Vec::new();
+
+    let titles_title_chars: Vec<Vec<char>> = titles
+        .into_iter()
+        .map(|t| t.chars().collect::<Vec<char>>())
+        .collect();
+    'chars: for i in 0..max {
+        let mut current_char_left: &char = titles_title_chars.get(0).unwrap().get(i).unwrap();
+        let mut current_char_right: &char = titles_title_chars
+            .get(0)
+            .unwrap()
+            .get(titles_title_chars[0].len() - i - 1)
+            .unwrap();
+
+        println!(
+            "c: {:1} ccl: {:2} ccr: {:3}",
+            i, current_char_left, current_char_right
+        );
+
+        'titles: for (t_i, title) in titles_title_chars.iter().enumerate() {
+            let ri = title.len() - i - 1;
+            println!(
+                "tc: {:1} t: {:2} l: {:3} r: {:4}",
+                i,
+                title.clone().iter().collect::<String>(),
+                title[i],
+                title[ri]
+            );
+            let right_match = &title[ri] == current_char_right;
+            let left_match = &title[i] == current_char_left;
+
+            if !left_match && !right_match {
+                // no more matches so stop looping
+                break 'chars;
+            }
+
+            if t_i == (titles.len() - 1) {
+                println!("last title");
+                if right_match {
+                    // right matched last char
+                    // note this puts the characters in reverse order because rev iter (end - i)
+                    matching_ends.push(&title[ri]);
+                }
+
+                if left_match {
+                    // left matched last char
+                    matching_starts.push(&title[i]);
+                }
+            }
+
+            current_char_right = &title[ri];
+            current_char_left = &title[i];
         }
+
+        println!(
+            "Text start {:?}",
+            matching_starts.clone().into_iter().collect::<String>()
+        );
+
+        println!(
+            "Text end {:?}",
+            matching_ends.clone().into_iter().collect::<String>()
+        );
     }
 
     let prefer_end = matching_starts.len() <= matching_ends.len();
@@ -86,9 +140,9 @@ fn titles_locate_matching_source(titles: &[&str]) -> TitleSource {
     };
 
     let source_name_len: usize = matching_ends.len();
-    let source_name: String = matching_ends.iter().rev().collect();
+    let source_name: String = matching_ends.into_iter().rev().collect();
     println!("Source name: {:?}", source_name);
-    let start_name: String = matching_starts.iter().collect();
+    let start_name: String = matching_starts.into_iter().collect();
     println!("Source name if start: {:?}", start_name);
 
     TitleSource {
